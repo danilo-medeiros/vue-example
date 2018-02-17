be34b4c083d9bffaecfc2247f3da621b5d790676
Danilo Medeiros <danilomedeiros03@gmail.com>
2018-02-17 01:15:32 -0300

    Alterações gerais

---
+++ form.js
@@ -0,0 +1,25 @@
+Vue.component("main-header", {
+	template: `<nav class="white" role="navigation">
+	<div class="nav-wrapper container">
+		<a id="logo-container" href="#" class="brand-logo"><img src="brasao2-mini.png"></a>
+		<ul id="nav-mobile" class="right hide-on-med-and-down">
+        	<li><span>Solicitação de cadastro de usuário no SGI</span></li>
+      	</ul>
+	</div>
+</nav>`
+});
+
+var app = new Vue({
+	el: "#app",
+	data: {
+		funcionario: {
+
+		},
+		page: 1
+	},
+	computed: {
+		isFirstPageComplete() {
+			return !!!this.funcionario.nome || !!!this.funcionario.cpf;
+		}
+	}
+})
